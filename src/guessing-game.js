3e5764305b17a04d1b537bac294a7ed4b0a6ce8c
Siarhei Prymakovich <prima.psa@gmail.com>
2019-10-11 18:17:27 +0300

    feat: implement guessing-game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
+        this.max = max;
+        this.min = min;        
     }
 
     guess() {
-
+      this.min = this.left;
+      this.max = this.right;
+      this.middle = Math.round((this.left + this.right)/2);
+      return this.middle;
     }
 
     lower() {
-
+      this.right = this.middle;
+      this.left = this.min;
     }
 
     greater() {
-
+      this.left = this.middle;
+      this.right = this.max;
     }
 }
 
